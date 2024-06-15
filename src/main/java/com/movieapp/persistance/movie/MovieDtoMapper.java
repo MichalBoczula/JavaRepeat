51de168b1ab7c8a25c0b436854ea4a8e7147be64
Michal Boczula <michal.boczula0101@gmail.com>
2024-06-15 23:40:59 +0200

    alter movie

--- src/main/java/com/movieapp/persistance/movie/MovieDtoMapper.java
+++ src/main/java/com/movieapp/persistance/movie/MovieDtoMapper.java
@@ -2,12 +2,15 @@ package com.movieapp.persistance.movie;
 
 import com.movieapp.domain.Movie;
 
-public class MovieDtoMapper {
+class MovieDtoMapper {
     static MovieDto map(Movie movie) {
         return new MovieDto(
                 movie.getId(),
                 movie.getTitle(),
                 movie.getOriginalTitle(),
+                movie.getShortDescription(),
+                movie.getDescription(),
+                movie.getYoutubeTrailerId(),
                 movie.getReleaseYear(),
                 movie.getGenre().getName(),
                 movie.isPromoted()
